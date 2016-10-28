e9804f23368edc31e0d7251e482ce7e744a07e93
Volodymyr Shcherbyna <vladimshc@gmail.com>
2016-10-28 18:44:41 +0300

    written function  sumTo

--- lesson_3/js/lesson3.js
+++ lesson_3/js/lesson3.js
@@ -1,13 +1,14 @@
-// // ---------Faktorial-----------
-// var testFaktorial_2 = factorial(-2);
-// var testFaktorial_1 = factorial(-1);
-// var testFaktorial0 = factorial(0);
-// var testFaktorial1 = factorial(1);
-// var testFaktorial2 = factorial(2);
-// var testFaktorial3 = factorial(3);
-// var testFaktorial4 = factorial(4);
-// var testFaktorial5 = factorial(5);
-// var testFaktorial6 = factorial(6);
+// // // ---------Faktorial-----------
+// console.log("Faktorial");
+// var testFaktorial_2 = factorialRecursion(-2);
+// var testFaktorial_1 = factorialRecursion(-1);
+// var testFaktorial0 = factorialRecursion(0);
+// var testFaktorial1 = factorialRecursion(1);
+// var testFaktorial2 = factorialRecursion(2);
+// var testFaktorial3 = factorialRecursion(3);
+// var testFaktorial4 = factorialRecursion(4);
+// var testFaktorial5 = factorialRecursion(5);
+// var testFaktorial6 = factorialRecursion(6);
 // console.log(testFaktorial_2);
 // console.log(testFaktorial_1);
 // console.log(testFaktorial0);
@@ -17,25 +18,48 @@
 // console.log(testFaktorial4);
 // console.log(testFaktorial5);
 // console.log(testFaktorial6);
-//
+// //
 // //----------Degree-------------
-// var testDegree_0 = extent(0, -8);
-// var testDegree_8 = extent(2, -8);
-// var testDegree0 = extent(2, 0);
-// var testDegree8 = extent(2, 8);
+// console.log("Degree");
+// var testDegree_0 = extentRecursion(0, -8);
+// var testDegree_8 = extentRecursion(2, -8);
+// var testDegree0 = extentRecursion(2, 0);
+// var testDegree8 = extentRecursion(2, 8);
 // console.log(testDegree_0);
 // console.log(testDegree_8);
 // console.log(testDegree0);
 // console.log(testDegree8);
+//
+// // ----------Sum Numbers-------------
+// console.log("Sum Numbers");
+// var testNumber1 = sumNumbersRecursion(3742);
+// var testNumber2 = sumNumbersRecursion(12345678912);
+// var testNumber3 = sumNumbersRecursion(0);
+// var testNumber4 = sumNumbersRecursion(-1);
+// console.log(testNumber1);
+// console.log(3+7+4+2);
+// console.log(testNumber2);
+// console.log(1+2+3+4+5+6+7+8+9+1+2);
+// console.log(testNumber3);
+// console.log(0);
+// console.log(testNumber4);
+// console.log(-1);
 
+// // ----------Sum To-------------
+// var testSumToRec = sumToRecursion(10000);
+// console.log(testSumToRec);
+// var testSumToCyc = sumToCycle(10000);
+// console.log(testSumToCyc);
+// var testSumToArithProgression = sumToArithProgression(10000);
+// console.log(testSumToArithProgression);
 
-function factorial(number) {
+function factorialRecursion(number) {
     var result;
     if (number < 0) {
         result = undefined;
     }
     else if (number > 1) {
-        result = number * factorial(number - 1);
+        result = number * factorialRecursion(number - 1);
     }
     else if (number = 1) {
         result = number;
@@ -46,7 +70,7 @@ function factorial(number) {
     return result;
 }
 
-function extent(number, deg) {
+function extentRecursion(number, deg) {
     var res;
     if (number === 0 && deg <= 0) {
         res = undefined;
@@ -56,43 +80,46 @@ function extent(number, deg) {
     }
     else if (deg < 0) {
         deg = deg * (-1);
-        res = 1 / (number * extent(number, deg - 1));
+        res = 1 / (number * extentRecursion(number, deg - 1));
     }
     else if (deg > 1) {
-        res = number * extent(number, deg - 1);
+        res = number * extentRecursion(number, deg - 1);
     } else res = number;
     return res;
 }
 
-function sumNumbers(number) {
+function sumNumbersRecursion(number) {
     var res = 0;
-
     if (number > 0){
         res = number % 10;
-        res = res + sumNumbers((number/10)^0);
+        res = res + sumNumbersRecursion((number/10)^0);
     }
     return res;
-
 }
 
-// var testNumber1 = sumNumbers(3742);
-var testNumber2 = sumNumbers(123456789123);
-// var testNumber3 = sumNumbers(0);
-// var testNumber4 = sumNumbers(-1);
-// var testNumber5 = sumNumbers(4476748946544644611468743547869966343456789);
-// var testNumber6 = sumNumbers(44767489465446446);
-// console.log(testNumber1);
-console.log(testNumber2);
-// console.log(testNumber3);
-// console.log(testNumber4);
-// console.log(testNumber5);
-// console.log(testNumber6);
-// console.log(3+7+4+2);
-console.log(1+2+3+4+5+6+7+8+9+1+2+3);
-// console.log(0);
-// console.log(-1);
-// console.log(4+4+7+6+7+4+8+9+4+6+5+4+4+6+4+4+6+1+1+4+6+8+7+4+3+5+4+7+8+6+9+9+6+6+3+4+3+4+5+6+7+8+9);
-// console.log(4+4+7+6+7+4+8+9+4+6+5+4+4+6+4+4+6);
+function sumToRecursion(n) {
+    var res = 0;
+    if (n >= 1) {
+       res = n + sumToRecursion(n - 1);
+    } else n = 0;
+    return res;
+}
+
+function sumToCycle(n) {
+    var res = 0;
+    for (var i = n; n > 0; n--){
+        res = res + n;
+    }
+    return res;
+}
+
+function sumToArithProgression(n) {
+    var res;
+    return res = (n + 1)/2*n;
+    
+}
+
+
 
 
 
