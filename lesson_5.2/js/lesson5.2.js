d3fd53c82196c0753423db3a7674dcd78d7d074e
Volodymyr Shcherbyna <vladimshc@gmail.com>
2016-11-14 04:35:13 +0200

    Task - 5.2Watter - is DONE

--- lesson_5.2/js/lesson5.2.js
+++ lesson_5.2/js/lesson5.2.js
@@ -1,44 +1,88 @@
 // var arr = [2,1,5,0,3,4,7,2,3,1,0]; //test = 10 ok
 // var arr = [1,4,0,1,2,3,1,0]; //test = 6 ok
-var arr = [0,1,5,3,1,2,4,3,4,2,5,2,1,3]; // =19 false
-function watter(arr) {
-    var arrSort = arr.slice();
-    var arr2 = arr.slice();
-    arrSort.sort();
+// var arr = [0,1,5,3,1,2,2,3,3,4,5,2,1,3]; // =20
+// var arr = [0,1,5,3,1,2,4,3,4,2,5,2,1,3]; // =19
 
-    var min = arrSort[0];
-    var max = arrSort[arrSort.length-1];
+function doIt() {
+    var arr = input();
+    var arrayWatter = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrayWatter[i] = 0;
+    }
+    var watter = 0;
+    //---------------------------get watter alhorithm---------------------------//
+    (function getWatter() {
+        var leftMax = 0;
+        var rightMax = 0;
+        var leftIndex = 0;
+        var rightIndex = arr.length - 1;
 
-    var result = 0;
-        for (var i = 0; i < arr.length; i++) {
-            var q1 = (arr2[i] > arr2[i + 1]);
-            if (q1) {
-                var s = 0;
-                var w = i;
-                while (arr2[w + 1] < arr2[w + 2]){
-                    s++;
-                    w++;
-                }
-                var m = arr2[i];
-                var n = arr2[i+s+1];
-
-
-                if (arr2[i] > arr2[i+s+1]){
-                    for (var r = i + 1; r <= i + s; r++) {
-                        arr2[r] = arr2[i+s+1];
-                    }
-                } else {
-                    for (var t = i + 1; t <= i + s; t++) {
-                        arr2[t] = arr2[i];
-                    }
-                }
+        while (leftIndex < rightIndex) {
+            if (arr[leftIndex] > leftMax) {
+                leftMax = arr[leftIndex];
+            }
+            if (arr[rightIndex] > rightMax) {
+                rightMax = arr[rightIndex];
+            }
+            if (leftMax >= rightMax) {
+                watter += rightMax - arr[rightIndex];
+                arrayWatter[rightIndex] = rightMax - arr[rightIndex];
+                rightIndex--;
+            } else {
+                watter += leftMax - arr[leftIndex];
+                arrayWatter[leftIndex] = leftMax - arr[leftIndex];
+                leftIndex++;
             }
         }
-    for (var j = 0; j < arr.length; j++){
-        result = result + arr2[j] - arr[j];
-    }
-console.log(result);
-return result;
+        return watter;
+    })();
+
+    print(arr, arrayWatter, watter);
 }
 
-watter(arr);
+function input() {
+    var input = document.getElementById("input");
+    var res = input.value.toString().split(",");
+    input.value = "";
+    return res;
+}
+
+function print(arr, arrWatter, watter) {
+    var string1 = document.getElementById("arrWatter");
+    var string2 = document.getElementById("amountWatter");
+    string1.innerText = "In array = " + arr;
+    string2.innerText = "Amount of watter = " + watter;
+
+    var x1 = 0;
+    var y1 = 0;
+    var table = document.getElementById("table");
+    for (var h = 0; h < arr.length; h++) {
+        var createDivX = document.createElement("div");
+        createDivX.classList.add("class-x");
+        createDivX.id = "x" + x1;
+        x1 = x1 + 1;
+        table.appendChild(createDivX);
+        while (arr[h] > 0) {
+            var createDivY = document.createElement("div");
+            createDivY.classList.add("brown");
+            createDivY.innerText = "X";
+            createDivY.id = "y" + y1;
+            y1 = y1 + 1;
+            createDivX.appendChild(createDivY);
+            arr[h] = arr[h] - 1;
+        }
+    }
+    for (var k = 0; k < arrWatter.length; k++) {
+        var findDivX = document.getElementById("x" + k);
+        while (arrWatter[k] > 0) {
+            var createWatterDivY = document.createElement("div");
+            createWatterDivY.classList.add("blue");
+            createWatterDivY.innerText = "W";
+            createWatterDivY.id = "y" + y1;
+            y1 = y1 + 1;
+            findDivX.appendChild(createWatterDivY);
+            arrWatter[k] = arrWatter[k] - 1;
+        }
+    }
+}
+
