ee91bb899f69d78f3383f25ef11774214a2da934
Volodymyr Shcherbyna <vladimshc@gmail.com>
2016-12-09 12:21:13 +0200

    add 'score', 'new game'

--- barley-break/js/barley-break.js
+++ barley-break/js/barley-break.js
@@ -1,10 +1,10 @@
 var useCheat = false;
+var scoreCount = 0;
 $(".win").hide();
 
 $(document).ready(function () {
     $(document).on("click", ".cell", move);
     $(document).on("click", ".cheat span", cheat);
-    // $(document).on("click", ".cheat-on", cheat);
     render();
 });
 
@@ -16,12 +16,12 @@ function render() {
         var a = Math.random()*15 + 1 | 0;
         if ($.inArray(a, arr) === -1) {
             arr.push(a);
-            $(".wrapper-content").append('<div class="cell" id="' + uuid + '">' + a + '</div>');
+            $(".content").append('<div class="cell" id="' + uuid + '">' + a + '</div>');
             i++;
             uuid++;
         }
     }
-    $(".wrapper-content").append('<div class="cell none-number" id="16"></div>');
+    $(".content").append('<div class="cell none-number" id="16"></div>');
 }
 
 function move(event) {
@@ -32,16 +32,24 @@ function move(event) {
     function moveCell() {
         $(".none-number").text(a).removeClass("none-number");
         $("#" + uuid).text("").addClass("none-number");
+
+    }
+
+    function score() {
+        scoreCount++;
+        $("#score-id").text(scoreCount);
     }
 
     if (useCheat) {
         moveCell();
+        score();
     } else {
         if   ( (uuid - 4) === resolution
             || (uuid - 1) === resolution
             || (uuid + 1) === resolution
             || (uuid + 4) === resolution) {
             moveCell();
+            score();
         }
     }
     sequence();
@@ -55,20 +63,29 @@ function sequence() {
     }
     if (arrSeq === arrTest.toString()){
         $(".win").show();
+        $(document).off("click", ".cell", move);
     }
 }
+
+function newGame(){
+    scoreCount = 0;
+    $("#score-id").text(scoreCount);
+    $(".content").empty();
+    render();
+}
+
 //---------------- cheat mode :))) -------------------//
 function cheat(event) {
     var className = event.target.className;
     if (className === "cheat-off"){
         $(".cheat-off").removeClass("cheat-off").addClass("cheat-on");
-        $(".wrapper-content").css("background-color", "#ffc4ba");
+        $(".content").css("background-color", "#ffc4ba");
         $(".cheat").css("background-color", "#ff2800");
         $(".cheat").css("color", "#ffffff");
         useCheat = true;
     } else {
         $(".cheat-on").removeClass("cheat-on").addClass("cheat-off");
-        $(".wrapper-content").css("background-color", "rgba(0,0,0,.1)");
+        $(".content").css("background-color", "rgba(0,0,0,.1)");
         $(".cheat").css("background-color", "#FDFEFE");
         $(".cheat").css("color", "#000000");
         useCheat = false;
